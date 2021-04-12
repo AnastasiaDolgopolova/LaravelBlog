7bf5c2c5e13923dbb9c2985f93b7727d3372aa4e
AnastasiaDolgopolova <anastasiadolgopolova11@gmail.com>
2021-04-12 12:59:31 +0300

    first commit
    creating a blog

---
+++ app/User.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const IS_ACTIVE = 0;
+    const IS_BANNED = 1;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public static function add($fields)
+    {
+        $user = new static;
+        $user->fill($fields);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields); //name,email
+        
+        $this->save();
+    }
+
+    public function generatePassword($password)
+    {
+        if($password != null)
+        {
+            $this->password = bcrypt($password);
+            $this->save();
+        }
+    }
+
+    public function remove()
+    {
+        $this->removeAvatar();
+        $this->delete();
+    }
+
+    public function uploadAvatar($image)
+    {
+        if($image == null) { return; }
+
+        $this->removeAvatar();
+        //dd(get_class_methods($image));//узнаем какие существуют методы у данного класса
+
+        $filename = Str :: random(10) . '.' . $image->extension();
+        $image->storeAs('uploads', $filename);
+        $this->avatar = $filename;
+        $this->save();
+    }
+
+    public function removeAvatar()
+    {
+        if($this->avatar != null)
+        {
+            Storage::delete('uploads/' . $this->avatar);
+        }
+    }
+
+    public function getAvatar()
+    {
+        if($this->avatar == null)
+        {
+            return '/img/no-user-image.png';
+        }
+        return '/uploads/' . $this->avatar;
+    }
+
+    public function makeAdmin()
+    {
+        $this->is_admin = 1;
+        $this->save();
+    }
+
+    public function makeNormal()
+    {
+        $this->is_admin = 0;
+        $this->save();
+    }
+
+    public function toggleAdmin($value)
+    {
+        if($value == null)
+        {
+            return $this->makeNormal();
+        }
+
+        return $this->makeAdmin();
+    }
+
+    public function ban()
+    {
+        $this->status = User::IS_BANNED;
+        $this->save();
+    }
+
+    public function unban()
+    {
+        $this->status = User::IS_ACTIVE;
+        $this->save();
+    }
+
+    public function toggleBan($value)
+    {
+        if($value == null)
+        {
+            return $this->unban();
+        }
+
+        return $this->ban();
+    }
+}
