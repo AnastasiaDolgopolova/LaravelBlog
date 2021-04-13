0d7a000ab325d70a5a84f70cbd9ca309ee7697b6
AnastasiaDolgopolova <anastasiadolgopolova11@gmail.com>
2021-04-13 10:26:30 +0300

    adding a frontend

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,8 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/* @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Post;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -24,5 +25,19 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->sentence,
+        'image' => 'photo1.png',
+        'date' => '08/09/21',
+        'views' => $faker->numberBetween(0,5000),
+        'category_id' => 4,
+        'user_id' => 1,
+        'status' => 1,
+        'is_featured' => 0
     ];
 });
