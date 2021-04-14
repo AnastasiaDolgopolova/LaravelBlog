585ada860aabaedf0137bd12b1f2b7e8c64491a5
AnastasiaDolgopolova <anastasiadolgopolova11@gmail.com>
2021-04-14 13:05:58 +0300

    customizing pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $posts = Post::paginate(3);
+        return view('pages.index',['posts' => $posts]);
+    }
+
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('pages.show', compact('post'));
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Error 404</title>
+
+    <link rel="stylesheet" href="/css/front.css">
+</head>
+
+<body>
+    <section id="wrapper" class="container-fluid mt-5">
+
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1 class="text-danger">404</h1>
+                <h1 class="text-danger">Page Not Found !</h1>
+
+                <a href="/" class="btn btn-info">Back</a>
+            </div>
+        </div>
+    </section>
+    <script src="/js/front.js"></script>
+</body>
+
+</html>
